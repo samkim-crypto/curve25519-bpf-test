668af86fd5ab575751091359a3596eaa5518ec8f
Sam Kim <skim13@cs.stanford.edu>
2021-06-02 10:40:31 -0400

    Add field.rs

---
+++ src/field.rs
@@ -0,0 +1,216 @@
+
+use crate::constants;
+
+/// A `FieldElement` represents an element of the field
+/// \\( \mathbb Z / (2\^{255} - 19)\\).
+///
+/// In the 64-bit implementation, a `FieldElement` is represented in
+/// radix \\(2\^{51}\\) as five `u64`s; the coefficients are allowed to
+/// grow up to \\(2\^{54}\\) between reductions modulo \\(p\\).
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct FieldElement(pub [u64; 5]);
+
+
+impl FieldElement {
+    /// Determine if this `FieldElement` is negative, in the sense
+    /// used in the ed25519 paper: `x` is negative if the low bit is
+    /// set.
+    ///
+    /// # Return
+    ///
+    /// If negative, return `Choice(1)`.  Otherwise, return `Choice(0)`.
+    pub fn is_negative(&self) -> bool {
+        let bytes = self.to_bytes();
+        bytes[0] & 1 == 1
+    }
+
+    /// Determine if this `FieldElement` is zero.
+    ///
+    /// # Return
+    ///
+    /// If zero, return `Choice(1)`.  Otherwise, return `Choice(0)`.
+    pub fn is_zero(&self) -> bool {
+        self.to_bytes() == [0u8; 32]
+    }
+
+    /// Compute (self^(2^250-1), self^11), used as a helper function
+    /// within invert() and pow22523().
+    fn pow22501(&self) -> (FieldElement, FieldElement) {
+        // Instead of managing which temporary variables are used
+        // for what, we define as many as we need and leave stack
+        // allocation to the compiler
+        //
+        // Each temporary variable t_i is of the form (self)^e_i.
+        // Squaring t_i corresponds to multiplying e_i by 2,
+        // so the pow2k function shifts e_i left by k places.
+        // Multiplying t_i and t_j corresponds to adding e_i + e_j.
+        //
+        // Temporary t_i                      Nonzero bits of e_i
+        //
+        let t0  = self.square();           // 1         e_0 = 2^1
+        let t1  = t0.square().square();    // 3         e_1 = 2^3
+        let t2  = self * &t1;              // 3,0       e_2 = 2^3 + 2^0
+        let t3  = &t0 * &t2;               // 3,1,0
+        let t4  = t3.square();             // 4,2,1
+        let t5  = &t2 * &t4;               // 4,3,2,1,0
+        let t6  = t5.pow2k(5);             // 9,8,7,6,5
+        let t7  = &t6 * &t5;               // 9,8,7,6,5,4,3,2,1,0
+        let t8  = t7.pow2k(10);            // 19..10
+        let t9  = &t8 * &t7;               // 19..0
+        let t10 = t9.pow2k(20);            // 39..20
+        let t11 = &t10 * &t9;              // 39..0
+        let t12 = t11.pow2k(10);           // 49..10
+        let t13 = &t12 * &t7;              // 49..0
+        let t14 = t13.pow2k(50);           // 99..50
+        let t15 = &t14 * &t13;             // 99..0
+        let t16 = t15.pow2k(100);          // 199..100
+        let t17 = &t16 * &t15;             // 199..0
+        let t18 = t17.pow2k(50);           // 249..50
+        let t19 = &t18 * &t13;             // 249..0
+
+        (t19, t3)
+    }
+
+    /// Given a slice of public `FieldElements`, replace each with its inverse.
+    ///
+    /// All input `FieldElements` **MUST** be nonzero.
+    #[cfg(feature = "alloc")]
+    pub fn batch_invert(inputs: &mut [FieldElement]) {
+        // Montgomery’s Trick and Fast Implementation of Masked AES
+        // Genelle, Prouff and Quisquater
+        // Section 3.2
+
+        let n = inputs.len();
+        let mut scratch = vec![FieldElement::one(); n];
+
+        // Keep an accumulator of all of the previous products
+        let mut acc = FieldElement::one();
+
+        // Pass through the input vector, recording the previous
+        // products in the scratch space
+        for (input, scratch) in inputs.iter().zip(scratch.iter_mut()) {
+            *scratch = acc;
+            acc = &acc * input;
+        }
+
+	// acc is nonzero iff all inputs are nonzero
+        assert_eq!(acc.is_zero().unwrap_u8(), 0);
+
+        // Compute the inverse of all products
+        acc = acc.invert();
+
+        // Pass through the vector backwards to compute the inverses
+        // in place
+        for (input, scratch) in inputs.iter_mut().rev().zip(scratch.into_iter().rev()) {
+            let tmp = &acc * input;
+            *input = &acc * &scratch;
+            acc = tmp;
+        }
+    }
+
+    /// Given a nonzero field element, compute its inverse.
+    ///
+    /// The inverse is computed as self^(p-2), since
+    /// x^(p-2)x = x^(p-1) = 1 (mod p).
+    ///
+    /// This function returns zero on input zero.
+    pub fn invert(&self) -> FieldElement {
+        // The bits of p-2 = 2^255 -19 -2 are 11010111111...11.
+        //
+        //                                 nonzero bits of exponent
+        let (t19, t3) = self.pow22501();   // t19: 249..0 ; t3: 3,1,0
+        let t20 = t19.pow2k(5);            // 254..5
+        let t21 = &t20 * &t3;              // 254..5,3,1,0
+
+        t21
+    }
+
+    /// Raise this field element to the power (p-5)/8 = 2^252 -3.
+    fn pow_p58(&self) -> FieldElement {
+        // The bits of (p-5)/8 are 101111.....11.
+        //
+        //                                 nonzero bits of exponent
+        let (t19, _) = self.pow22501();    // 249..0
+        let t20 = t19.pow2k(2);            // 251..2
+        let t21 = self * &t20;             // 251..2,0
+
+        t21
+    }
+
+//     /// Given `FieldElements` `u` and `v`, compute either `sqrt(u/v)`
+//     /// or `sqrt(i*u/v)` in constant time.
+//     ///
+//     /// This function always returns the nonnegative square root.
+//     ///
+//     /// # Return
+//     ///
+//     /// - `(Choice(1), +sqrt(u/v))  ` if `v` is nonzero and `u/v` is square;
+//     /// - `(Choice(1), zero)        ` if `u` is zero;
+//     /// - `(Choice(0), zero)        ` if `v` is zero and `u` is nonzero;
+//     /// - `(Choice(0), +sqrt(i*u/v))` if `u/v` is nonsquare (so `i*u/v` is square).
+//     ///
+//     pub fn sqrt_ratio_i(u: &FieldElement, v: &FieldElement) -> (bool, FieldElement) {
+//         // Using the same trick as in ed25519 decoding, we merge the
+//         // inversion, the square root, and the square test as follows.
+//         //
+//         // To compute sqrt(α), we can compute β = α^((p+3)/8).
+//         // Then β^2 = ±α, so multiplying β by sqrt(-1) if necessary
+//         // gives sqrt(α).
+//         //
+//         // To compute 1/sqrt(α), we observe that
+//         //    1/β = α^(p-1 - (p+3)/8) = α^((7p-11)/8)
+//         //                            = α^3 * (α^7)^((p-5)/8).
+//         //
+//         // We can therefore compute sqrt(u/v) = sqrt(u)/sqrt(v)
+//         // by first computing
+//         //    r = u^((p+3)/8) v^(p-1-(p+3)/8)
+//         //      = u u^((p-5)/8) v^3 (v^7)^((p-5)/8)
+//         //      = (uv^3) (uv^7)^((p-5)/8).
+//         //
+//         // If v is nonzero and u/v is square, then r^2 = ±u/v,
+//         //                                     so vr^2 = ±u.
+//         // If vr^2 =  u, then sqrt(u/v) = r.
+//         // If vr^2 = -u, then sqrt(u/v) = r*sqrt(-1).
+//         //
+//         // If v is zero, r is also zero.
+// 
+//         let v3 = &v.square()  * v;
+//         let v7 = &v3.square() * v;
+//         let mut r = &(u * &v3) * &(u * &v7).pow_p58();
+//         let check = v * &r.square();
+// 
+//         let i = &constants::SQRT_M1;
+// 
+//         let correct_sign_sqrt = check == *u;
+//         let flipped_sign_sqrt = check == -u;
+//         let flipped_sign_sqrt_i = check == &(-u)*i;
+// 
+//         let r_prime = &constants::SQRT_M1 * &r;
+// 
+//         r.conditional_assign(&r_prime, flipped_sign_sqrt | flipped_sign_sqrt_i);
+// 
+//         // Choose the nonnegative square root.
+//         let r_is_negative = r.is_negative();
+//         r.conditional_negate(r_is_negative);
+// 
+//         let was_nonzero_square = correct_sign_sqrt | flipped_sign_sqrt;
+// 
+//         (was_nonzero_square, r)
+//     }
+// 
+//     /// Attempt to compute `sqrt(1/self)` in constant time.
+//     ///
+//     /// Convenience wrapper around `sqrt_ratio_i`.
+//     ///
+//     /// This function always returns the nonnegative square root.
+//     ///
+//     /// # Return
+//     ///
+//     /// - `(Choice(1), +sqrt(1/self))  ` if `self` is a nonzero square;
+//     /// - `(Choice(0), zero)           ` if `self` is zero;
+//     /// - `(Choice(0), +sqrt(i/self))  ` if `self` is a nonzero nonsquare;
+//     ///
+//     pub fn invsqrt(&self) -> (bool, FieldElement) {
+//         FieldElement::sqrt_ratio_i(&FieldElement::one(), self)
+//     }
+}
